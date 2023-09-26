584ae29e44da3231976ee1cbc47f807876d755ec
Pournima Gaikwad <87952332+Pournima2496@users.noreply.github.com>
2023-09-26 15:08:19 +0530

    Update Loading.jsx

--- src/layouts/loading/Loading.jsx
+++ src/layouts/loading/Loading.jsx
@@ -5,12 +5,6 @@ import "./loading.scss";
 const Loading = () => {
   return (
     <motion.div className="loading">
-      <motion.span
-        initial={{ y: -40 }}
-        animate={{ y: 0 }}
-        transition={{ duration: 0.6, delay: 0.6 }}
-        className="bouncing-ball"
-      ></motion.span>
       <h1>Geeks Invention</h1>
     </motion.div>
   );
